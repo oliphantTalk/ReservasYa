63ff4198f41679484a2284cd8c68795c8416c0b7
Nahuel-Barrena <nahuelbarrena@despegar.com>
2018-10-10 20:08:19 -0300

    fixing and startin with spring boot

---
+++ reservasYa/src/main/java/com/ttps/reservasYa/reservasYa/ReservasYaApplication.java
@@ -0,0 +1,12 @@
+package com.ttps.reservasYa.reservasYa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasYaApplication.class, args);
+	}
+}
